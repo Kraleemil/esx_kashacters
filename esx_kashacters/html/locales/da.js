b200f72ec0a9539e3314da7aada88d13ebd91ca6
Kraleemil <64910903+Kraleemil@users.noreply.github.com>
2021-10-14 16:26:11 +0200

    Create da.js

---
+++ esx_kashacters/html/locales/da.js
@@ -0,0 +1,14 @@
+const translate = new Object();
+
+translate.name = "Navn";
+translate.job = "Job";
+translate.bank = "Bank";
+translate.money = "Kontakter";
+translate.gender = "Køn";
+translate.dob = "Fødselsdato";
+translate.new = "";
+translate.delete = "SLET";
+translate.play = "SPIL";
+translate.playNew = "Opret Karakter";
+translate.modalTitle = "Er du sikker?";
+translate.modalText = "Ved at slette din karakter kan vi ikke gendanne den, alle ejendomme, biler osv. Slettes";
